31fe7b4e1383604b89b09a44cc5f179d4247922b
VictorCunha26 <vicentinivictor130@gmail.com>
2025-10-07 13:31:32 -0300

    commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # app.py
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import subprocess
 from flask_cors import CORS
+
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
@@ -14,5 +16,29 @@ def index():
 def reconhecer():
     subprocess.Popen(["python", "identificar.py"])
     return jsonify({"status": "iniciado"})
+
+# UPLOAD DE IMAGE
+
+UPLOAD_FOLDER = 'faces'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#Criando a pasta se ela não existir
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+#ROTA
+
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "Nenhum arquivo selecionado", 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return "Nenhum arquivo selecionado", 400
+
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return f"Arquivo {file.filename} salvo em faces/"
 
 app.run(port=5000)
--- static/script.js
+++ static/script.js
@@ -1,12 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const input = document.getElementById("fileInpu");
-  if (input) {
-    input.addEventListener("change", function() {
-      console.log("Arquivo selecionado!");
-    });
-  }
-});
+document.getElementById("upload").addEventListener("change", function(){
+    let file = this.files[0];
+    let formData = new FormData();
+    formData.append("file", file);
 
+    fetch("/upload", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => alert(data))
+    .catch(err => console.error(err));
+});
 
     function reconhecer() {
       fetch('http://127.0.0.1:5000/reconhecer')   // <<-- porta do BACKEND
